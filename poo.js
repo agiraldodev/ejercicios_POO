c440db9d4375d252c9302bdcf6dc12197605b155
Andrés Giraldo <31672123+agiraldodev@users.noreply.github.com>
2024-03-02 12:24:48 -0500

    Concepto de objeto usando js

---
+++ poo.js
@@ -0,0 +1,62 @@
+// Crear objetos
+
+/*
+let vehiculo = {
+
+    // Propiedad
+    color: 'rojo',
+    marca: 'chevrolet',
+    modelo: 2020,
+
+    // Métodos
+    pitar() {
+        console.log('Pi pi piiiiii!!!!!');
+    },
+    acelerar() {
+        console.log('Estoy acelerando');
+    }
+}
+
+console.log(vehiculo.pitar);
+console.log(vehiculo.acelerar);
+
+
+let cursos = ["sistemas", "ingles", "contaduria"];
+
+let estudiante = {
+    nombre: 'Stiven',
+    edad: 19,
+    sexo: 'masculino',
+    cursos: ["sistemas", "electrónica", "administracion de empresas"]
+}
+
+// Crear un objeto llamado estudiante donde sus propiedades sean
+//   nombre, edad, sexo y cursos. Los cursos pueden ser varios
+console.log(estudiante.cursos);
+*/
+
+let almacen = [
+    {
+        marca: 'iPhone',
+        precio: 1500000,
+        garantia: true,
+        colores: {
+            opcion1: 'Metalizado',
+            opcion2: 'Dorado',
+            opcion3: 'Negro'
+        },
+        almacenamiento: '500 GB',
+        camaraFrontal: true,
+        resolucionCamaraFrontal: '100 Mpx',
+    },
+    {
+        marca: 'Samsung',
+        precio: 2000000,
+        garantia: false,
+        colores: {
+            opcion1: 'Rosado',
+            opcion2: 'Azul',
+            opcion3: 'Blanco'
+        } 
+    }
+];
